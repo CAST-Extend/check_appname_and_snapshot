e2cce107dc28f7255fb68f1a1cb3bf8c251b45a0
sharath9620 <119046105+sharath9620@users.noreply.github.com>
2023-08-11 12:56:06 +0530

    Update check_app_and_snapshot.py

--- check_app_and_snapshot.py
+++ check_app_and_snapshot.py
@@ -1,16 +1,16 @@
 import json
-from requests.auth import HTTPBasicAuth
 import requests
 from argparse import ArgumentParser
 
-def check_appname():
-    method = "get"
-    url=f"{args.restURL}/api/applications"
-    auth = HTTPBasicAuth(f'{args.user}', f'{args.password}')
+def check_appname(restURL, api_key, application):
+    url=f"{restURL}/api/applications"
+    headers = {
+        "x-api-key": api_key
+    }
 
     try:
         #fetching the Application list and details.
-        rsp = requests.request(method, url, auth=auth)
+        rsp = requests.get(url, headers=headers)
         # print(rsp.status_code)
         if rsp.status_code == 200:
             apps = json.loads(rsp.text) 
@@ -18,7 +18,7 @@ def check_appname():
             for app in apps['applications']:
                 app_dict[app["name"]] = app["guid"]
 
-            if args.application not in app_dict.keys():
+            if application not in app_dict.keys():
                 return 1
             else:
                 return 0
@@ -34,12 +34,10 @@ def check_appname():
 if __name__ == "__main__":
 
     parser = ArgumentParser()
-    parser.add_argument('-restURL','--restURL',required=True,help='CAST REST API URL')
-    parser.add_argument('-user','--user',required=True,help='CAST REST API User Name')
-    parser.add_argument('-password','--password',required=True,help='CAST REST API Password')
+    parser.add_argument('-restURL','--restURL',required=True,help='CAST Console REST API URL')
+    parser.add_argument('-api_key','--api_key',required=True,help='CAST Console API KEY')
     parser.add_argument('-application','--application',required=True,help='Application Name')
 
     args=parser.parse_args()
-    # aip = AipRestCall(args.restURL, args.user, args.password)
-    result = check_appname()
+    result = check_appname(args.restURL, args.api_key, args.application)
     print(result)
